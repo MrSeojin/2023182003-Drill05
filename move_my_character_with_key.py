6aafb61b8d68c70c534cfe35aa0efcbd5023404e
Seojin.K <7701seojin@gmail.com>
2024-09-30 19:02:26 +0900

    배경& 종료

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -5,14 +5,23 @@ back_ground = load_image('TUK_GROUND.png')
 character = load_image('my_character.png')
 
 def handle_events():
-    # 이벤트 처리
-    pass
+    global running
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_ESCAPE:
+                running = False
 
 # 전역 변수
 running = True
 
 while running:
-    # 그리기
-    pass
+    clear_canvas()
+    back_ground.clip_draw(0, 0, 1280, 1024, 400, 300, 800, 600)
+    update_canvas()
+    handle_events()
+    delay(0.05)
 
 close_canvas()
