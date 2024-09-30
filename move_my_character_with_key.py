558e103479abcbb3c263231b7567220f2b0cbd4d
Seojin.K <7701seojin@gmail.com>
2024-09-30 18:40:54 +0900

    pico2d

---
+++ move_my_character_with_key.py
@@ -0,0 +1,18 @@
+from pico2d import*
+
+open_canvas()
+back_ground = load_image('TUK_GROUND.png')
+character = load_image('my_character.png')
+
+def handle_events():
+    # 이벤트 처리
+    pass
+
+# 전역 변수
+running = True
+
+while running:
+    # 그리기
+    pass
+
+close_canvas()
